e1f38441fcea488e0251b58e68f11f81a5105993
Diego Olivera <diegolivera93@gmail.com>
2025-06-17 00:51:44 -0300

    task/ Se agrego una configuracion para que solo se pueda subir una sola imagen y con un maximo de 5mb

--- backend/middlewares/upload.middleware.js
+++ backend/middlewares/upload.middleware.js
@@ -25,7 +25,7 @@ const fileFilter = (req, file, cb) => {
     }
 }
 
-const upload = multer({storage,fileFilter})
+const upload = multer({storage, fileFilter, limits: { fileSize: 5 * 1024 * 1024 } }).single('image');
 
 module.exports = {
     upload,
