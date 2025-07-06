bd9d1bab78cd90a8d08f2b3f7afc117592328f59
Diego Olivera <diegolivera93@gmail.com>
2025-07-06 18:59:53 -0300

    task/se creo el endpoint y el front para que el super admin pueda ver los tickets con las compras

--- backend/repository/saleRepository.js
+++ backend/repository/saleRepository.js
@@ -1,9 +1,27 @@
+
 const { Sale } = require('../models');
 
 
 const createSale = (saleData) => Sale.create(saleData);
 
+const getAllSales = () => Sale.findAll({
+    attributes: ['ticket_code', 'customer_name', 'total', 'created_at'],
+    include:[
+        {
+            association: 'SaleDetails',
+            attributes: ['quantity', 'subtotal'],
+            include: [
+                {
+                    association: 'Product',
+                    attributes: ['name', 'price']
+                }
+            ]
+        }
+
+    ] 
+})
 
 module.exports = {
     createSale,
+    getAllSales
 }
